3c6e6af9823d352c056b6821dcd1b6c4937dba77
SemkiShow <mgdeveloper123@gmail.com>
2025-12-20 17:46:45 +0100

    Fix a fix of UI scaling

--- src/UI.cpp
+++ src/UI.cpp
@@ -14,12 +14,12 @@
 bool showIslandsBoxes = false;
 
 Vector2 startWindowSize = windowSize;
-#define UI_SPACING 30 * windowSize.y / startWindowSize.y
-#define ELEMENT_SIZE_X 30 * windowSize.x / startWindowSize.x
-#define ELEMENT_SIZE_Y 30 * windowSize.y / startWindowSize.y
-#define ELEMENT_SPACING 10 * windowSize.y / startWindowSize.y
-#define SLIDER_WIDTH (startWindowSize.x - 270) * windowSize.x / startWindowSize.x
-#define BUTTON_WIDTH ELEMENT_SIZE_X * 10
+#define UI_SPACING 30
+#define ELEMENT_SIZE 30
+#define ELEMENT_SPACING 10
+#define SLIDER_WIDTH windowSize.x - 270
+#define BUTTON_WIDTH ELEMENT_SIZE * 10 * windowSize.x / startWindowSize.x
+#define FONT_SIZE 24
 
 bool isSettings = false;
 int islandEditIdx = -1;
@@ -28,39 +28,38 @@ float nextElementPositionY = UI_SPACING * 2;
 
 void DrawCheckBox(const char* text, bool* value)
 {
-    GuiCheckBox(Rectangle{UI_SPACING * 2, nextElementPositionY, ELEMENT_SIZE_X, ELEMENT_SIZE_Y},
-                text, value);
-    nextElementPositionY += ELEMENT_SIZE_Y + ELEMENT_SPACING;
+    GuiCheckBox(Rectangle{UI_SPACING * 2, nextElementPositionY, ELEMENT_SIZE, ELEMENT_SIZE}, text,
+                value);
+    nextElementPositionY += ELEMENT_SIZE + ELEMENT_SPACING;
 }
 
 void DrawSliderInt(const char* leftText, const char* rightText, int* value, float minValue,
                    float maxValue)
 {
-    float fontSize = 24;
-    float sliderHeight = fontSize + 7;
     float valueFloat = *value;
-    GuiSlider({UI_SPACING * 2, nextElementPositionY, SLIDER_WIDTH, sliderHeight}, leftText,
+    GuiSlider({UI_SPACING * 2, nextElementPositionY, SLIDER_WIDTH, ELEMENT_SIZE}, leftText,
               rightText, &valueFloat, minValue, maxValue);
     *value = valueFloat;
     DrawText(std::to_string(*value).c_str(), (SLIDER_WIDTH + UI_SPACING * 2) / 2.f,
-             nextElementPositionY + 5, fontSize, WHITE);
-    nextElementPositionY += sliderHeight + ELEMENT_SPACING;
+             nextElementPositionY + 5, FONT_SIZE, WHITE);
+    nextElementPositionY += ELEMENT_SIZE + ELEMENT_SPACING;
 }
 
 void DrawTextCentered(const char* text, int fontSize)
 {
     fontSize = fontSize * windowSize.y / startWindowSize.y;
-    DrawText(text, (windowSize.x - MeasureText(text, fontSize)) / 2, nextElementPositionY,
-             fontSize, WHITE);
-    nextElementPositionY += fontSize + ELEMENT_SPACING;
+    DrawText(text, (windowSize.x - MeasureText(text, fontSize)) / 2, nextElementPositionY, fontSize,
+             WHITE);
+    nextElementPositionY += fontSize + ELEMENT_SPACING * windowSize.y / startWindowSize.y;
 }
 
 int DrawButtonCentered(const char* text)
 {
-    int res = GuiButton({windowSize.x / 2 - BUTTON_WIDTH / 2.0f, nextElementPositionY, BUTTON_WIDTH,
-                         ELEMENT_SIZE_Y},
-                        text);
-    nextElementPositionY += ELEMENT_SIZE_Y + ELEMENT_SPACING;
+    float buttonHieght = ELEMENT_SIZE * windowSize.y / startWindowSize.y;
+    int res = GuiButton(
+        {windowSize.x / 2 - BUTTON_WIDTH / 2.0f, nextElementPositionY, BUTTON_WIDTH, buttonHieght},
+        text);
+    nextElementPositionY += buttonHieght + ELEMENT_SPACING * windowSize.y / startWindowSize.y;
     return res;
 }
 
@@ -75,8 +74,8 @@ void DrawSettings()
 
     {
         auto buttonRec = rec;
-        buttonRec.width = ELEMENT_SIZE_X;
-        buttonRec.height = ELEMENT_SIZE_Y;
+        buttonRec.width = ELEMENT_SIZE;
+        buttonRec.height = ELEMENT_SIZE;
         buttonRec.x += rec.width - UI_SPACING;
         if (GuiButton(buttonRec, "#113#")) isSettings = false;
     }
@@ -91,8 +90,8 @@ void EditIsland()
 
     {
         auto buttonRec = rec;
-        buttonRec.width = ELEMENT_SIZE_X;
-        buttonRec.height = ELEMENT_SIZE_Y;
+        buttonRec.width = ELEMENT_SIZE;
+        buttonRec.height = ELEMENT_SIZE;
         buttonRec.x += rec.width - UI_SPACING;
         if (GuiButton(buttonRec, "#113#")) islandEditIdx = -1;
     }
@@ -105,8 +104,7 @@ void DrawGameUI()
 {
     if (showFPS) DrawFPS(0, 0);
 
-    if (GuiButton(Rectangle{windowSize.x - ELEMENT_SIZE_X, 0, ELEMENT_SIZE_X, ELEMENT_SIZE_Y},
-                  "#142#"))
+    if (GuiButton(Rectangle{windowSize.x - ELEMENT_SIZE, 0, ELEMENT_SIZE, ELEMENT_SIZE}, "#142#"))
         isSettings = !isSettings;
 
     // if (GuiButton(Rectangle{windowSize.x - ELEMENT_SIZE * 2, 0, ELEMENT_SIZE, ELEMENT_SIZE},
@@ -125,7 +123,7 @@ void DrawMainUI()
     nextElementPositionY = UI_SPACING;
 
     DrawTextCentered("Colony Simulator", 48);
-    nextElementPositionY += (ELEMENT_SIZE_Y + ELEMENT_SPACING) * 2;
+    nextElementPositionY += (ELEMENT_SIZE + ELEMENT_SPACING) * 2 * windowSize.y / startWindowSize.y;
     if (DrawButtonCentered("Play")) OpenGameMenu();
     if (DrawButtonCentered("Settings")) isSettings = !isSettings;
     if (DrawButtonCentered("About")) std::cout << "[TODO]: Add an about menu\n";
